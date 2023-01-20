e97f0379c67c21512e9c974eb3131a1f1fe5e24c
Kacper Chmielewski <kchmielewski@tlen.pl>
2023-01-20 16:50:58 +0100

    Rails App re-installed

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ProfileWeb
+module Profile
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
